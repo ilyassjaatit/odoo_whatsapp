39cb00a5c45d040eba2117c5fc96fe160ec6d86c
Ilyass Jaatit <ilyassjaatit@gmail.com>
2024-01-18 16:12:22 +0100

    addon base Whatsapp

---
+++ whatsapp/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Whatsapp gateway',
+    'version': '0.0.1',
+    'category': 'Hidden/Tools',
+    'summary': 'Whatsapp Text Messaging',
+    'description': """
+    This module gives a framework for Whatsapp text messaging
+    ----------------------------------------------------
+    """,
+    'depends': [
+        'base',
+        'iap_mail',
+        'mail',
+        'phone_validation'
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': True,
+    'assets': {
+        
+    },
+    'license': 'LGPL-3',
+}
