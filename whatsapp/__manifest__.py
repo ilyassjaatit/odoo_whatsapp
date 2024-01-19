62dfc7736076a9a24b86590ca7ada867c2953885
Ilyass Jaatit <ilyassjaatit@gmail.com>
2024-01-19 20:10:06 +0100

    whatsapp/models/

--- whatsapp/__manifest__.py
+++ whatsapp/__manifest__.py
@@ -1,26 +1,17 @@
 {
-    'name': 'Whatsapp gateway',
-    'version': '0.0.1',
-    'category': 'Hidden/Tools',
-    'summary': 'Whatsapp Text Messaging',
-    'description': """
-    This module gives a framework for Whatsapp text messaging
-    ----------------------------------------------------
-    """,
-    'depends': [
-        'base',
-        'iap_mail',
-        'mail',
-        'phone_validation'
+    "name": "Whatsapp gateway",
+    "version": "0.0.1",
+    "category": "Hidden/Tools",
+    "summary": "Whatsapp Text Messaging",
+    "description": """This module gives a framework for Whatsapp text messaging----------------------------------------------------""",
+    "depends": ["base", "iap_mail", "mail", "phone_validation"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/whatsapp_whatsapp_views.xml",
     ],
-    'data': [
-    ],
-    'demo': [
-    ],
-    'installable': True,
-    'auto_install': True,
-    'assets': {
-        
-    },
-    'license': 'LGPL-3',
+    "demo": [],
+    "installable": True,
+    "auto_install": True,
+    "assets": {},
+    "license": "LGPL-3",
 }
